875c223445ffa03e9b0dbd264877e8b4128a2a87
Flavio Ferreira <flafjr@gmail.com>
2024-12-15 23:45:55 -0700

    Fixing unit test and printing all env variables

--- src/main/java/com/github/flafjr/Main.java
+++ src/main/java/com/github/flafjr/Main.java
@@ -1,13 +1,17 @@
 package com.github.flafjr;
 
 import io.quarkus.runtime.annotations.QuarkusMain;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
 import io.quarkus.runtime.Quarkus;
-
-
 @QuarkusMain
 public class Main {
     public static void main(String... args) {
         System.out.println("Running main method");
+        Config config = ConfigProvider.getConfig();
+        System.out.println("Application Properties:");
+        config.getPropertyNames()
+                .forEach(name -> System.out.println(name + " = " + config.getOptionalValue(name, String.class).orElse("empty")));
         Quarkus.run(args);
     }
 }
